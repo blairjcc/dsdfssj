1fd54186919ae733d85e2b866053dfe1a351cd64
blairjcc <131194454+blairjcc@users.noreply.github.com>
2023-04-27 14:13:59 -0400

    Add image
    Updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,15 +2,20 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in JCC')
+def foo():
+	print('bar')
+
+st.set_page_config(layout='wide')
+st.title('Streamlit Test App')
+
+st.subheader('Interactive Widgets')
 
 st.button('Click me')
-st.experimental_data_editor('Edit data', data)
 st.checkbox('I agree')
 st.radio('Pick one', ['cats', 'dogs'])
 st.selectbox('Pick one', ['cats', 'dogs'])
 st.multiselect('Buy', ['milk', 'apples', 'potatoes'])
-st.slider('Pick a number', 0, 100)
+st.slider('Pick a number', 0, 100,50)
 st.select_slider('Pick a size', ['S', 'M', 'L'])
 st.text_input('First name')
 st.number_input('Pick a number', 0, 10)
@@ -26,11 +31,29 @@ if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(data)
 
-st.subheader('Number of pickups by hour')
+st.subheader('Text Items')
 
+st.text('Fixed width text')
+st.markdown('_Markdown_') # see *
+st.latex(r''' e^{i\pi} + 1 = 0 ''')
+st.write('Most objects') # df, err, func, keras!
+st.write(['st', 'is <', 3]) # see *
+st.title('My title')
+st.header('My header')
+st.subheader('My sub')
+st.code('for i in range(8): foo()')
 
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
+st.subheader('Media Items')
+st.image('./doggo-unsplash.jpp')
+st.audio(data)
+st.video(data)
+link = '[GitHub](http://github.com)'
+st.markdown(link, unsafe_allow_html=True)
+
+st.subheader('Side Bar')
+with st.sidebar:
+	st.radio('Select one:', [
+
 
 
 st.subheader('Map of all pickups at %s:00' % hour_to_filter)
